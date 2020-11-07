364d7110313d4b5db2c6ab0a5a640563dfeeb7bc
dgasull <dgasull@bsc.es>
2020-11-07 14:28:24 +0100

    Fixing wrong sed

--- .misc/release.sh
+++ .misc/release.sh
@@ -13,18 +13,18 @@ find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:develop/bscdataclay\/
 find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:develop/bscdataclay\/dspython/g' {} \;
 find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:develop/bscdataclay\/client/g' {} \;
 	 
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule\:develop-alpine/bscdataclay\/logicmodule\:alpine/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:develop-alpine/bscdataclay\/dsjava\:alpine/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:develop-alpine/bscdataclay\/dspython\:alpine/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:develop-alpine/bscdataclay\/client\:alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule\:-alpine/bscdataclay\/logicmodule\:alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:-alpine/bscdataclay\/dsjava\:alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:-alpine/bscdataclay\/dspython\:alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:-alpine/bscdataclay\/client\:alpine/g' {} \;
 
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule\:develop-slim/bscdataclay\/logicmodule\:slim/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:develop-slim/bscdataclay\/dsjava\:slim/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:develop-slim/bscdataclay\/dspython\:slim/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:develop-slim/bscdataclay\/client\:slim/g' {} \;
-	 
-find $BASEDIR -type f -exec sed -i 's/module load DATACLAY\/develop/module load DATACLAY/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/module load COMPSs\/Trunk/module load COMPSs/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule\:-slim/bscdataclay\/logicmodule\:slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:-slim/bscdataclay\/dsjava\:slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:-slim/bscdataclay\/dspython\:slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:-slim/bscdataclay\/client\:slim/g' {} \;
+
+find $BASEDIR -type f -exec sed -i 's/module load DATACLAY/develop\/develop/module load DATACLAY/develop/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/module load COMPSs/Trunk\/Trunk/module load COMPSs/Trunk/g' {} \;
 
 git add -A
 git commit -m "Modified docker images"
@@ -40,18 +40,18 @@ find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava/bscdataclay\/dsjava\:d
 find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython/bscdataclay\/dspython\:develop/g' {} \;
 find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client/bscdataclay\/client\:develop/g' {} \;
 
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule/bscdataclay\/logicmodule\:develop-alpine/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava/bscdataclay\/dsjava\:develop-alpine/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython/bscdataclay\/dspython\:develop-alpine/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client/bscdataclay\/client\:develop-alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule\:alpine/bscdataclay\/logicmodule\:develop-alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:alpine/bscdataclay\/dsjava\:develop-alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:alpine/bscdataclay\/dspython\:develop-alpine/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:alpine/bscdataclay\/client\:develop-alpine/g' {} \;
 
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule/bscdataclay\/logicmodule\:develop-slim/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava/bscdataclay\/dsjava\:develop-slim/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython/bscdataclay\/dspython\:develop-slim/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client/bscdataclay\/client\:develop-slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/logicmodule\:slim/bscdataclay\/logicmodule\:develop-slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dsjava\:slim/bscdataclay\/dsjava\:develop-slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/dspython\:slim/bscdataclay\/dspython\:develop-slim/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/bscdataclay\/client\:slim/bscdataclay\/client\:develop-slim/g' {} \;
 
-find $BASEDIR -type f -exec sed -i 's/module load DATACLAY/module load DATACLAY\/develop/g' {} \;
-find $BASEDIR -type f -exec sed -i 's/module load COMPSs/module load COMPSs\/Trunk/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/module load DATACLAY/develop/module load DATACLAY/develop\/develop/g' {} \;
+find $BASEDIR -type f -exec sed -i 's/module load COMPSs/Trunk/module load COMPSs/Trunk\/Trunk/g' {} \;
 
 git add -A
 git commit -m "New development version"

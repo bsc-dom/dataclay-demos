88f58728029a55ae895532f1fbbf623ab64cb8a1
Marc Monfort <marc.monfort@gmail.com>
2022-03-21 23:14:57 +0100

    Change estrae version

--- hpc/hellopeople-python/dataclay_job.sh
+++ hpc/hellopeople-python/dataclay_job.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -e
 #SBATCH --job-name=dataclaydemo
-#SBATCH --output=job-%A-%a.out
-#SBATCH --error=job-%A-%a.out
+#SBATCH --output=job-%A.out
+#SBATCH --error=job-%A.out
 #SBATCH --nodes=3
 #SBATCH --time=00:10:00
 #SBATCH --exclusive 
@@ -11,7 +11,8 @@
 # Configurations
 NETWORK_SUFFIX="-ib0"
 module load DATACLAY/DevelMarc
-module load EXTRAE/3.8.3
+module load gcc/7.2.0
+module load EXTRAE/3.5.4
 
 # --- SLURM ---- 
 JOB_HOSTS=$(scontrol show hostname $SBATCH_JOB_NODELIST)
@@ -24,23 +25,22 @@ export DATACLAY_JOBID=$SLURM_JOBID
 # Run demo 
 
 # Deploy dataClay
-echo $'\e[1;32m' "dataclaysrv" $'\e[0m'
-dataclaysrv start --hosts "$HOSTS" --debug $EXTRA_ARGS
+echo $'\e[1;33m' "dataclaysrv" $'\e[0m'
+dataclaysrv start --hosts "$HOSTS" $EXTRA_ARGS
 
 # Register accounts, model and store stubs in client node
-echo $'\e[1;32m' "dataclayprepare" $'\e[0m'
+echo $'\e[1;33m' "dataclayprepare" $'\e[0m'
 dataclayprepare $(pwd)/model/src $(pwd)/app/src DemoNS python 
 
 # Run app 
 # pyclay <main> <args>
-echo $'\e[1;32m' "pyclay" $'\e[0m'
-# pyclay src/hellopeople.py forthepeople martin 33 $EXTRA_ARGS
+echo $'\e[1;33m' "pyclay" $'\e[0m'
 source $HOME/.dataClay/$SLURM_JOBID/client.config
 python app/src/hellopeople.py forthepeople martin 33
 
 # Stop dataClay 
-echo $'\e[1;32m' "stop" $'\e[0m'
-dataclaysrv stop
+echo $'\e[1;33m' "stop" $'\e[0m'
+dataclaysrv stop $EXTRA_ARGS
 
 echo $'\e[1;32m' "DEMO SUCCESSFULLY FINISHED!" $'\e[0m'
 
